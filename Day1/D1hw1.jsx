4f5382e73104e3d3b43ae4b6bb12dff1f81c5d12
RasikaHari <150768711+RasikaHari@users.noreply.github.com>
2024-08-06 00:08:13 +0530

    Add files via upload

---
+++ Day1/D1hw1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Team from './D1hw11';
+function World(){
+    return (
+        <div>
+            <h1>Welcome to the World Component</h1>
+            <Team />
+        </div>
+    )
+}
+export default World;
